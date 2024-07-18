882cbf34b06aee7c3ecc6be9cbc3c9ba119bf8a1
beom33 <byby5546@naver.com>
2024-07-18 16:06:01 +0900

    7.18일 수업 (JSON,타임리프 포함)

--- 11.프레임 워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11.프레임 워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -46,10 +46,11 @@ public class MvcConfig implements WebMvcConfigurer {
         return joinValidator;
     } */
 
+    /*
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
-    }
+    } */
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
