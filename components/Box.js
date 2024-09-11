4609bdf4929b0681261f8084bdae6408b688a86b
beom33 <byby5546@naver.com>
2024-09-11 10:14:11 +0900

    수업내용

--- components/Box.js
+++ components/Box.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
-const Box = ({rounded}) => {
-  return <View style={[styles.box, rounded && styles.rounded]} />;
+const Box = ({rounded, size}) => {
+  return <View style={[styles.box, rounded && styles.rounded, styles[size]]} />;
 };
 
 const styles = StyleSheet.create({
@@ -15,6 +15,18 @@ const styles = StyleSheet.create({
   rounded: {
     borderRadius: 20,
   },
+   small : {
+    width: 33,
+    height: 32
+   },
+    medium: {
+      width: 65,
+      height: 65,
+    },
+    large: {
+      width: 33,
+      height: 33,
+    }
 });
 
 export default Box;
